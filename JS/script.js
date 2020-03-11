dd07f54d9b8662baf37b8d8c5e32380b681a4bcf
Yulms <yminsk@gmail.com>
2020-03-11 15:32:20 +0300

    Сверстана шапка

---
+++ JS/script.js
@@ -0,0 +1,34 @@
+"use srict";
+
+// Double Range Slider
+let rangeSlider = document.getElementById("range-slider");
+noUiSlider.create(rangeSlider, {
+  range: {
+    min: [1],
+    max: [1000]
+  },
+  start: [1, 1000],
+  margin: 0, // минимальная разница
+  connect: true, //цветной центр
+  step: 1,
+  format: {
+    to: function(value) {
+      return parseInt(value);
+    },
+    from: function(value) {
+      return parseInt(value);
+    }
+  }
+});
+let inputFrom = document.getElementById("input-from");
+let inputTo = document.getElementById("input-to");
+let inputs = [inputFrom, inputTo];
+rangeSlider.noUiSlider.on("update", function(values, handle) {
+  inputs[handle].value = values[handle];
+});
+inputFrom.addEventListener("change", function() {
+  rangeSlider.noUiSlider.set([this.value, null]);
+});
+inputTo.addEventListener("change", function() {
+  rangeSlider.noUiSlider.set([null, this.value]);
+});
