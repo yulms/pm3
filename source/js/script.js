27112727d4046bd8a33e51507dc3242b2e56c1b1
Yulms <yminsk@gmail.com>
2020-10-09 18:04:41 +0300

    Добавляет текстовое поле как в material design

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,7 @@ import Modal from './_modal.js';
 import HeaderTopMenu from './_header__top-menu.js';
 import * as quantityInput from './_quantity-input.js';
 import initTooltips from './_tooltip.js';
+import initTextfields from './_textfield.js';
 
 
 function initModals() {
@@ -40,3 +41,4 @@ initHeaderTopMenu();
 initModals();
 quantityInput.init();
 initTooltips();
+initTextfields();
