a25674ff3dfd48ea16e0682aa5495809f94d9eaa
Yulms <yminsk@gmail.com>
2020-10-10 23:04:49 +0300

    adding gh-pages package

--- gulp_tasks/deploy-to-github.js
+++ gulp_tasks/deploy-to-github.js
@@ -1,11 +1,24 @@
 'use strict';
 
-const gulp = require('gulp');
-const ghPages = require('gulp-gh-pages');
+const ghPages = require('gh-pages');
+const path = require('path');
 
-function deploy() {
-  return gulp.src('./build/**/*')
-    .pipe(ghPages());
+
+function deploy(cb) {
+  ghPages.publish(path.join(process.cwd(), './build'), cb);
 }
 
-module.exports = deploy;
+module.exports.deploy = deploy;
+
+
+
+
+// const gulp = require('gulp');
+// const ghPages = require('gulp-gh-pages');
+
+// function deploy() {
+//   return gulp.src('./build/**/*')
+//     .pipe(ghPages());
+// }
+
+// module.exports = deploy;
