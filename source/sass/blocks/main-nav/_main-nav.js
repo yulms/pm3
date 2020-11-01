69aafb4ac3c9cfa45388a0e629ad67492cc0e919
Yulms <yminsk@gmail.com>
2020-11-01 16:12:15 +0300

    Добавляет пагинацию

--- source/sass/blocks/main-nav/_main-nav.js
+++ source/sass/blocks/main-nav/_main-nav.js
@@ -1,4 +1,5 @@
-import appendCustomFocusEvents from './custom-focus-events.js';
+// Определить поддержку hover
+// Если поддержки нет - блокировать первый клик по меню первого уровня
 
 
 class MainNav {
@@ -11,86 +12,44 @@ class MainNav {
 
     Object.assign(this, defaults, overrides);
 
-
-    this._state = {
-      _isOpened: false,
-      _focusIsInside: false,
-      _mouseIsInside: false
-    };
-
-    this._mainElement = document.querySelector(this.mainSelector);
-    this._mainListElement = this._mainElement.querySelector(this.mainListSelector);
-
-    this._addOpenedStateEvents();
-
-    this._mainElement.addEventListener('openMenu', () => {
-      this._state._isOpened = true;
-    });
-
-    this._mainElement.addEventListener('closeMenu', () => {
-      this._state._isOpened = false;
-    });
-
-    this._addFirstClickChecking();
-  }
+    this._mainElement = null;
+    this._firstClickedElement = null;
 
 
-  _addOpenedStateEvents() {
-    appendCustomFocusEvents(this._mainElement);
-
-    this._mainElement.addEventListener('focusEnter', () => {
-      this._state._focusIsInside = true;
-      // сначала дем отработать click
-      setTimeout(this._dispatchOpenEvent.bind(this), 0);
-    });
-
-    this._mainElement.addEventListener('focusLeave', () => {
-      this._state._focusIsInside = false;
-      this._dispatchCloseEvent();
-    });
-
-    this._mainListElement.addEventListener('mouseenter', () => {
-      this._state._mouseIsInside = true;
-      // сначала дем отработать click
-      setTimeout(this._dispatchOpenEvent.bind(this), 0);
-    });
-
-    this._mainListElement.addEventListener('mouseleave', () => {
-      this._state._mouseIsInside = false;
-      this._dispatchCloseEvent();
-    });
-  }
-
-
-  _dispatchOpenEvent() {
-    if (this._state._isOpened) return;
-    let openEvent = new CustomEvent('openMenu');
-    this._mainElement.dispatchEvent(openEvent);
-  }
-
-
-  _dispatchCloseEvent() {
-    if (this._state._isOpened) {
-      if (!this._state._focusIsInside && !this._state._mouseIsInside) {
-        let closeEvent = new CustomEvent('closeMenu');
-        if (!this._state._focusIsInside) {
-          this._mainElement.dispatchEvent(closeEvent);
-        }
-      }
+    if (this._detectTouchSupport()) {
+      this._init();
+      this._enableFirstClickPrevention();
     }
   }
 
+  _init() {
+    this._mainElement = document.querySelector(this.mainSelector);
+  }
 
-  _addFirstClickChecking() {
-    this._mainListElement.addEventListener('click', (evt) => {
+  _detectTouchSupport() {
+    let isTouchSupport = true;
+    try {
+      document.createEvent('TouchEvent');
+    }
+    catch (err) {
+      isTouchSupport = false;
+    }
+    return isTouchSupport;
+  }
+
+  _enableFirstClickPrevention() {
+    this._mainElement.addEventListener('click', (evt) => {
       let target = evt.target.closest(this.firstLevelLinkSelector);
       if (!target) return;
 
-      if (!this._state._isOpened) {
+      if (this._firstClickedElement !== target) {
         evt.preventDefault();
       }
+
+      this._firstClickedElement = target;
     });
   }
+
 }
 
 
