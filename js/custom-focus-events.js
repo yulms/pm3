3a70051c272970448d87b02c0e5c06bee50ac8df
Yulms <yminsk@gmail.com>
2020-11-01 14:35:33 +0300

    Updates

--- js/custom-focus-events.js
+++ js/custom-focus-events.js
@@ -9,24 +9,38 @@ function appendCustomFocusEvents(containerElement) {
 
   function _onContainerFocusIn() {
     if (!focusInside) {
-      containerElement.dispatchEvent(new CustomEvent('focusEnter'));
+      containerElement.dispatchEvent(new CustomEvent('focusEnter', {bubbles: true}));
       focusInside = true;
     }
   }
 
-  function  _onContainerFocusOut() {
-    if (!containerElement.contains(document.activeElement)) {
-      containerElement.dispatchEvent(new CustomEvent('focusLeave'));
+  function _onContainerFocusOut(evt) {
+    if (!containerElement.contains(evt.relatedTarget)) {
+      containerElement.dispatchEvent(new CustomEvent('focusLeave', {
+        bubbles: true,
+        detail: {
+          relatedTarget: evt.relatedTarget
+        }
+      }));
       focusInside = false;
     }
   }
 
+  // в старой версии использовалось отложенный focusout
+  // function _onContainerFocusOut() {
+  //   if (!containerElement.contains(document.activeElement)) {
+  //     containerElement.dispatchEvent(new CustomEvent('focusLeave', {bubbles: true}));
+  //     focusInside = false;
+  //   }
+  // }
+
   if (!containerElement.customFocusEventsIsAdded) {
     containerElement.addEventListener('focusin', _onContainerFocusIn);
-    containerElement.addEventListener('focusout', () => {
-      setTimeout(_onContainerFocusOut, 0);
-    });
+    containerElement.addEventListener('focusout', _onContainerFocusOut);
     containerElement.customFocusEventsIsAdded = true;
+    // containerElement.addEventListener('focusout', (evt) => {
+    //   setTimeout(_onContainerFocusOut, 0);
+    // });
   }
 }
 
