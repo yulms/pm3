682442e5da23361bd68e73739ff3b6d3be514a65
Yulms <yminsk@gmail.com>
2020-11-06 21:52:35 +0300

    Updates

--- js/_modal.js
+++ js/_modal.js
@@ -44,183 +44,191 @@ class Modal {
 
     Object.assign(this, defaults, overrides);
 
-    this._init();
+    // this._init();
+    this._modals = [];
+    this._documentKeydownHandlerWasAdded = false;
+
+    this.handleEvent = (evt) => {
+      switch(evt.type) {
+        case 'keydown':
+          isEscapePressEvent(evt, this._closeAfterAnnimationEnd.bind(this));
+          break;
+      }
+    };
+
 
 
     document.addEventListener('click', (evt) => {
-      let target = evt.target.closest('[' + this.triggerDataAttributeName + ']');
-      if (!target) return;
+      let triggerElement = evt.target.closest('[' + this.triggerDataAttributeName + ']');
+      if (!triggerElement) return;
 
       evt.preventDefault();
-      this._triggerElement = target;
-      let modalContentSelector = this._triggerElement.getAttribute(this.triggerDataAttributeName);
-      this._open(modalContentSelector);
+      this._open(triggerElement);
     });
 
   }
 
 
-  _open(modalContentSelector) {
-
+  _open(triggerElement) {
     const _cloneModalTemplate = () => {
       return document.querySelector(this.modalTemplateSelector)
         .content.querySelector(this.modalBodySelector)
         .cloneNode(true);
     };
 
-    const _readPositionAttribute = () => {
-      return this._triggerElement.dataset[this.modalTypeDataAttribute] || this.defaultPosition;
-    };
+    const _addPositionClass = (currentModal) => {
+      const _readPositionAttribute = () => {
+        return triggerElement.dataset[this.modalTypeDataAttribute] || this.defaultPosition;
+      };
 
-    const _addPositionClass = () => {
       let modalPosition = _readPositionAttribute();
-      this._modalElement.classList.add(ModalPositionClasses[modalPosition]);
+      currentModal._modalElement.classList.add(ModalPositionClasses[modalPosition]);
     };
 
-    const _saveHomelandPosition = () => {
-      this._modalContentElementHomelandPosition = {
-        parentElement: this._modalContentElement.parentElement,
-        nextElement: this._modalContentElement.nextElementSibling
+    const _saveHomelandPosition = (currentModal) => {
+      currentModal._modalContentElementHomelandPosition = {
+        parentElement: currentModal._modalContentElement.parentElement,
+        nextElement: currentModal._modalContentElement.nextElementSibling
       };
     };
 
-    const _changeAriaAttrIfNeeded = () => {
-      if (this._modalContentElement.getAttribute('aria-hidden')) {
-        this._modalContentElement.setAttribute('aria-hidden', false);
-        this._ariaAttrWasRemoved = true;
+    const _changeAriaAttrIfNeeded = (currentModal) => {
+      if (currentModal._modalContentElement.getAttribute('aria-hidden')) {
+        currentModal._modalContentElement.setAttribute('aria-hidden', false);
+        currentModal._ariaAttrWasRemoved = true;
       }
     };
 
-    const _show = () => {
-      if (this._modalContentElement.classList.contains(this.closeModalClass)) {
-        this._modalContentElement.classList.remove(this.closeModalClass);
-        this._closedClassWasRemoved = true;
+    const addOverlay = (currentModal) => {
+      currentModal._overlayElement = document.createElement('div');
+      currentModal._overlayElement.classList.add(this.overlaySelector);
+      currentModal._overlayElement.classList.add(this.modalOverlaySelector);
+      currentModal._modalElement.append(currentModal._overlayElement);
+    };
+
+    const _show = (currentModal) => {
+      if (currentModal._modalContentElement.classList.contains(this.closeModalClass)) {
+        currentModal._modalContentElement.classList.remove(this.closeModalClass);
+        currentModal._closedClassWasRemoved = true;
       } else {
         // у него нет класса закрытия (модальное расположено в контенте)
         // могут быть проблемы, если надо flex или grid.
         // При необходимости добавить значение по умолчанию и указание необходимого стиля в data атрибуте
-        this._modalContentElement.style.display = 'block';
+        currentModal._modalContentElement.style.display = 'block';
       }
     };
 
-    const _addHandlers = () => {
-      this._onDocumentKeydown = (evt) => {
-        isEscapePressEvent(evt, this._closeAfterAnnimationEnd.bind(this));
-      };
+    const _addHandlers = (currentModal) => {
+      let closeButtonElement = currentModal._modalElement.querySelector(this.closeButtonSelector);
+      closeButtonElement.addEventListener('click', this._closeAfterAnnimationEnd.bind(this));
 
-      this._onCloseButtonClick = () => this._closeAfterAnnimationEnd();
+      currentModal._overlayElement.addEventListener('click', this._closeAfterAnnimationEnd.bind(this));
 
-
-      this._overlayElement.addEventListener('click', this._closeAfterAnnimationEnd.bind(this));
-      document.addEventListener('keydown', this._onDocumentKeydown);
-      this._closeButtonElement = this._modalElement.querySelector(this.closeButtonSelector);
-      if (this._closeButtonElement) {
-        this._closeButtonElement.addEventListener('click', this._onCloseButtonClick);
+      // обработчик на закрытие по Esc - один на все открытые окна
+      if (!this._documentKeydownHandlerWasAdded) {
+        console.log('добавляем обработчик закрытия');
+        document.addEventListener('keydown', this);  // см this.handleEvent
+        this._documentKeydownHandlerWasAdded = true;
       }
     };
 
 
-    this._modalElement = _cloneModalTemplate();
-    _addPositionClass();
-    this._modalContentElement = document.querySelector(modalContentSelector);
-    _saveHomelandPosition();
-    _changeAriaAttrIfNeeded();
-    this._modalElement.children[0].prepend(this._modalContentElement);
-    document.body.append(this._modalElement);
-    _show();
-    this._toggleOverlay();
-    _addHandlers();
-    scrollLock({lock: true});
-    this._focusContol();
-    this._isOpened = true;
+    this._modals.push({});
+    let currentModal = this._modals[this._getLastModalIndex()];
+
+    currentModal._triggerElement = triggerElement;
+    currentModal._modalElement = _cloneModalTemplate();
+    _addPositionClass(currentModal);
+
+    let modalContentSelector = triggerElement.getAttribute(this.triggerDataAttributeName);
+    currentModal._modalContentElement = document.querySelector(modalContentSelector);
+
+    // сохранение позиции в DOM, из которой будет выдернут HTML внутренностей окна
+    _saveHomelandPosition(currentModal);
+    _changeAriaAttrIfNeeded(currentModal);
+
+    currentModal._modalElement.children[0].prepend(currentModal._modalContentElement);
+    addOverlay(currentModal);
+    document.body.append(currentModal._modalElement);
+    _show(currentModal);
+    _addHandlers(currentModal);
+
+    // блокируем только на первом окне
+    if (this._modals.length === 1) {
+      scrollLock({lock: true});
+    }
+
+    this._focus(currentModal, {shouldBeInside: true});
+  }
+
+
+  _closeAfterAnnimationEnd () {
+    let currentModal = this._modals[this._getLastModalIndex()];
+    executeAfterAnimationEnd(
+      {
+        element: currentModal._modalElement,
+        animationClass: this.animationOnCloseClass,
+        animationName: this.animationOnCloseName,
+        callback: this._close.bind(this)
+      });
   }
 
 
   _close() {
     const _removeHandlers = () => {
-      document.removeEventListener('keydown', this._onDocumentKeydown);
-      if (this._closeButtonElement) {
-        this._closeButtonElement.removeEventListener('click', this._onCloseButtonClick);
+      // удаляем обработчик по keydown только если закрывается единственное окно
+      if (this._modals.length === 1) {
+        document.removeEventListener('keydown', this);
+        this._documentKeydownHandlerWasAdded = false;
       }
     };
 
-    this._modalContentElement.style.display = '';
 
-    if (this._closedClassWasRemoved) {
-      this._modalContentElement.classList.add(this.closeModalClass);
+    let currentModal = this._modals[this._getLastModalIndex()];
+    currentModal._modalContentElement.style.display = '';
+
+    if (currentModal._closedClassWasRemoved) {
+      currentModal._modalContentElement.classList.add(this.closeModalClass);
     }
-    if (this._ariaAttrWasRemoved) {
-      this._modalContentElement.setAttribute('aria-hidden', true);
+
+    if (currentModal._ariaAttrWasRemoved) {
+      currentModal._modalContentElement.setAttribute('aria-hidden', true);
     }
 
-    let {parentElement, nextElement} =  this._modalContentElementHomelandPosition;
-    parentElement.insertBefore(this._modalContentElement, nextElement);
+    let {parentElement, nextElement} =  currentModal._modalContentElementHomelandPosition;
+    parentElement.insertBefore(currentModal._modalContentElement, nextElement);
 
-    this._modalElement.remove();
-    this._toggleOverlay();
+    currentModal._modalElement.remove();
+
     _removeHandlers();
-    scrollLock({lock: false});
-    this._focusContol();
-    this._init();
-    this._isOpened = false;
-  }
 
+    this._modals.pop();
 
-  _closeAfterAnnimationEnd() {
-
-    executeAfterAnimationEnd(
-      {
-        element: this._modalElement,
-        animationClass: this.animationOnCloseClass,
-        animationName: this.animationOnCloseName,
-        callback: this._close.bind(this)
-      });
-
-  }
-
-
-  _toggleOverlay() {
-    if (!this._isOpened) {
-      // if (this.blurClassName) {
-      //   document.body.classList.add(this.blurClassName);
-      // }
-      this._overlayElement = document.createElement('div');
-      this._overlayElement.classList.add(this.overlaySelector);
-      this._overlayElement.classList.add(this.modalOverlaySelector);
-      this._modalElement.append(this._overlayElement);
-    } else {
-      // if (this.blurClassName) {
-      //   document.body.classList.remove(this.blurClassName);
-      // }
-      this._overlayElement.remove();
+    if (this._modals.length === 0) {
+      scrollLock({lock: false});
     }
+
+    this._focus(currentModal, {shouldBeInside: false});
   }
 
 
-  _focusContol() {
+  _getLastModalIndex() {
+    return this._modals.length - 1;
+  }
+
+
+
+  _focus(currentModal, {shouldBeInside}) {
     // Метод переносит фокус с элемента открывающего окно
     // в само окно, и обратно, когда окно закрывается
-
-    const nodes = this._modalElement.querySelectorAll(this.focusElements);
-    if (this._isOpened && this._triggerElement) {
-      this._triggerElement.focus();
-    } else {
+    const nodes = currentModal._modalElement.querySelectorAll(this.focusElements);
+    if (shouldBeInside) {
       if (nodes.length) nodes[0].focus();
+    } else {
+      currentModal._triggerElement.focus();
     }
   }
 
-
-  _init() {
-    this._isOpened = false;
-    this._closedClassWasRemoved = false;
-    this._ariaAttrWasRemoved = false;
-    this._triggerElement = null;
-    this._modalElement = null;
-    this._closeButtonElement = null;
-    this._overlayElement = null;
-    this._modalContentElement = null;
-    this._modalContentElementHomelandPosition = null;
-  }
 }
 
 
