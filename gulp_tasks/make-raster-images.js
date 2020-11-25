4bc5c4776bf60fdb1516477917eca3df59980681
Yulms <yminsk@gmail.com>
2020-11-25 19:54:01 +0300

    Добавляет адаптивную графику

--- gulp_tasks/make-raster-images.js
+++ gulp_tasks/make-raster-images.js
@@ -3,6 +3,7 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
+const flatten = require('gulp-flatten');
 
 
 function minifyRasterImages() {
@@ -11,19 +12,22 @@ function minifyRasterImages() {
   ], {
     base: 'source'
   })
+    .pipe(flatten())
     .pipe(imagemin([
       imagemin.mozjpeg({
+        quality: 80,
         progressive: true
       }),
       imagemin.optipng({
         optimizationLevel: 3
       })
     ]))
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('build/img/raster'));
 }
 
 function makeWebP () {
   return gulp.src('source/img/raster/**/*.{png,jpg}')
+    .pipe(flatten())
     .pipe(webp({
       quality: 80
     }))
