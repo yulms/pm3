4bf588f7a03602b1c6344970d602cf99d6fccc24
Yulms <yminsk@gmail.com>
2020-11-27 15:26:52 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+import initLazyload from './lazyload.js';
 import initSwitchView from './_switch-view.js';
 import initMainNav from './_main-nav.js';
 import initBoundedModals from './_modal--bounded.js';
@@ -13,6 +14,7 @@ import initShowmores from './_showmore.js';
 import initStars from './_stars.js';
 
 const projectObjects = { // eslint-disable-line
+  lazyLoad: initLazyload(),
   switchView: initSwitchView(),
   mainNav: initMainNav(),
   modalsBounded: initBoundedModals(),
